7f0eab27bcde4cc2198a718a2b7634e9c66d156b
jonanct <49182949+jonanct@users.noreply.github.com>
2025-03-03 16:49:31 -0800

    Initial commit

---
+++ src/main/java/com/unir_app/buscador_service/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.unir_app.buscador_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://unir-app-jonathan-castros-projects-92c6cc81.vercel.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
